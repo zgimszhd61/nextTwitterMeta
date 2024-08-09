4a6ac733b09e81eb80b89ce5997d4bc209245d83
xiaoming <lininruc@gmail.com>
2024-08-09 21:46:54 +0800

    add mvs

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ const Home = () => {
       <Head>
         <title>我的应用程序</title>
         <meta name="description" content="这是一个示例应用程序" />
-        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="我的应用程序" />
         <meta name="twitter:description" content="这是一个示例应用程序" />
         <meta name="twitter:image" content="https://ppl-ai-public.s3.amazonaws.com/static/img/pplx-default-preview.png" />
