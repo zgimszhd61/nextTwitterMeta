37d6df698670655151499700fd8d25b1a53c947c
xiaoming <lininruc@gmail.com>
2024-08-09 21:38:59 +0800

    initial

---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+import Head from 'next/head';
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>我的应用程序</title>
+        <meta name="description" content="这是一个示例应用程序" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content="我的应用程序" />
+        <meta name="twitter:description" content="这是一个示例应用程序" />
+        <meta name="twitter:image" content="https://ppl-ai-public.s3.amazonaws.com/static/img/pplx-default-preview.png" />
+        <meta name="twitter:site" content="@seclink" />
+      </Head>
+      <h1>Hello world</h1>
+    </>
+  );
+};
+
+export default Home;
