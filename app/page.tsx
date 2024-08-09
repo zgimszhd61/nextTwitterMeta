f9bf62ba196851b3abe19118955e7e152c2a77bb
xiaoming <lininruc@gmail.com>
2024-08-09 21:55:05 +0800

    fix bug

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,23 @@
-import Head from 'next/head';
+import { NextSeo } from 'next-seo';
 
 const Home = () => {
   return (
     <>
-      <Head>
-        <title>我的应用程序</title>
-        <meta name="description" content="这是一个示例应用程序" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="我的应用程序" />
-        <meta name="twitter:description" content="这是一个示例应用程序" />
-        <meta name="twitter:image" content="https://ppl-ai-public.s3.amazonaws.com/static/img/pplx-default-preview.png" />
-        <meta name="twitter:site" content="@seclink" />
-      </Head>
+      <NextSeo
+        title="我的应用程序"
+        description="这是一个示例应用程序"
+        twitter={{
+          cardType: 'summary_large_image',
+          site: '@seclink',
+        }}
+        openGraph={{
+          images: [
+            {
+              url: 'https://ppl-ai-public.s3.amazonaws.com/static/img/pplx-default-preview.png',
+            },
+          ],
+        }}
+      />
       <h1>Hello world</h1>
     </>
   );
